736bee012a7f2b3e1f42b87ee15fc3550bd5cda5
laakeloussama716-sudo <laakeloussama716@gmail.com>
2026-07-01 02:41:00 +0100

    Implement full auth system, market rack, and dual sticks control

--- server.js
+++ server.js
@@ -2,77 +2,142 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const http = require("http").createServer(app);
-const io = require("socket.io")(http, {
-    cors: { origin: "*" }
-});
+const io = require("socket.io")(http, { cors: { origin: "*" } });
 
 app.use(express.static(path.join(__dirname)));
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
+app.get("/", (req, res) => { res.sendFile(path.join(__dirname, "index.html")); });
 
 let players = {};
-let registeredNames = {}; 
+let rooms = {}; // ????? ????? ??????? ??????? ????????
 
 io.on("connection", (socket) => {
-    console.log("Player connected: " + socket.id);
+    console.log("User connected: " + socket.id);
 
-    socket.on("newPlayer", (data) => {
-        const { x, y, angle, nickname, email } = data;
+    socket.on("joinGame", (data) => {
+        const { nickname, email, mode, gear } = data;
         
-        let nameExists = Object.values(registeredNames).includes(nickname);
-        let ownsName = registeredNames[email] === nickname;
-
-        if (nameExists && !ownsName) {
-            socket.emit("nameRejected", "This nickname is already taken by another account!");
-            return;
-        }
-
-        registeredNames[email] = nickname;
-
         players[socket.id] = {
             id: socket.id,
-            x: x,
-            y: y,
-            angle: angle,
+            x: Math.random() * 1000 + 500,
+            y: Math.random() * 1000 + 500,
+            angle: 0,
             nickname: nickname,
             email: email,
-            hp: 100
+            hp: 100 + (gear.armor * 25), // ????? ???? ??? ?????
+            maxHp: 100 + (gear.armor * 25),
+            gear: gear,
+            mode: mode,
+            room: null
         };
 
-        socket.emit("nameAccepted");
-        io.emit("stateUpdate", players);
+        // ???? ????? ???????? ??? ????? ??? ????? ???????
+        if (mode === "1v1") {
+            let joinedRoom = false;
+            for (let rId in rooms) {
+                if (rooms[rId].mode === "1v1" && rooms[rId].players.length < 2) {
+                    rooms[rId].players.push(socket.id);
+                    players[socket.id].room = rId;
+                    joinedRoom = true;
+                    break;
+                }
+            }
+            if (!joinedRoom) {
+                let rId = "room_1v1_" + socket.id;
+                rooms[rId] = { mode: "1v1", players: [socket.id] };
+                players[socket.id].room = rId;
+            }
+        } else if (mode === "br") {
+            let joinedRoom = false;
+            for (let rId in rooms) {
+                if (rooms[rId].mode === "br" && rooms[rId].players.length < 30) {
+                    rooms[rId].players.push(socket.id);
+                    players[socket.id].room = rId;
+                    joinedRoom = true;
+                    break;
+                }
+            }
+            if (!joinedRoom) {
+                let rId = "room_br_" + socket.id;
+                rooms[rId] = { mode: "br", players: [socket.id] };
+                players[socket.id].room = rId;
+            }
+        } else if (mode === "coop") {
+            let joinedRoom = false;
+            for (let rId in rooms) {
+                if (rooms[rId].mode === "coop" && rooms[rId].players.length < 2) {
+                    rooms[rId].players.push(socket.id);
+                    players[socket.id].room = rId;
+                    joinedRoom = true;
+                    break;
+                }
+            }
+            if (!joinedRoom) {
+                let rId = "room_coop_" + socket.id;
+                rooms[rId] = { mode: "coop", players: [socket.id], goldShared: 0, level: 1 };
+                players[socket.id].room = rId;
+            }
+        }
+
+        socket.join(players[socket.id].room);
+        socket.emit("joinedRoomSuccess", { roomId: players[socket.id].room });
+        sendRoomState(players[socket.id].room);
     });
 
     socket.on("move", (data) => {
-        if (players[socket.id]) {
+        if (players[socket.id] && players[socket.id].room) {
             players[socket.id].x = data.x;
             players[socket.id].y = data.y;
             players[socket.id].angle = data.angle;
-            io.emit("stateUpdate", players);
+            sendRoomState(players[socket.id].room);
         }
     });
 
     socket.on("shoot", (bulletData) => {
-        socket.broadcast.emit("bulletFired", bulletData);
+        if (players[socket.id] && players[socket.id].room) {
+            socket.to(players[socket.id].room).emit("bulletFired", bulletData);
+        }
+    });
+
+    socket.on("coopGoldUpdate", (data) => {
+        if (players[socket.id] && players[socket.id].room) {
+            let rId = players[socket.id].room;
+            if (rooms[rId]) {
+                rooms[rId].goldShared += data.amount;
+                io.to(rId).emit("coopGoldSynced", { goldShared: rooms[rId].goldShared });
+            }
+        }
     });
 
     socket.on("playerDamage", (data) => {
-        if (players[data.id]) {
-            players[data.id].hp = Math.max(0, players[data.id].hp - data.damage);
-            io.emit("stateUpdate", players);
+        let targetId = data.id;
+        if (players[targetId] && players[targetId].room) {
+            players[targetId].hp = Math.max(0, players[targetId].hp - data.damage);
+            sendRoomState(players[targetId].room);
         }
     });
 
     socket.on("disconnect", () => {
-        console.log("Player disconnected: " + socket.id);
-        delete players[socket.id];
-        io.emit("stateUpdate", players);
+        if (players[socket.id]) {
+            let rId = players[socket.id].room;
+            if (rId && rooms[rId]) {
+                rooms[rId].players = rooms[rId].players.filter(id => id !== socket.id);
+                if (rooms[rId].players.length === 0) delete rooms[rId];
+                else sendRoomState(rId);
+            }
+            delete players[socket.id];
+        }
+        console.log("User disconnected: " + socket.id);
     });
 });
 
+function sendRoomState(roomId) {
+    if (!rooms[roomId]) return;
+    let roomPlayers = {};
+    rooms[roomId].players.forEach(pId => {
+        if (players[pId]) roomPlayers[pId] = players[pId];
+    });
+    io.to(roomId).emit("stateUpdate", roomPlayers);
+}
+
 const PORT = process.env.PORT || 10000;
-http.listen(PORT, () => {
-    console.log("Server running on port: " + PORT);
-});
+http.listen(PORT, () => { console.log("Server running on port: " + PORT); });
